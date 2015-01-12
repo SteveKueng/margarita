0070a9d498266685f5e754f7c394f0f4862ce052
SteveKueng <kuengst@StevesMacBookPro.local>
2015-01-12 08:12:22 +0100

    fix

--- updates/views.py
+++ updates/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import get_object_or_404, redirect, render
 from reposadolib import reposadocommon
 
 def update_list(request):
-	branches = reposadocommon.getCatalogBranches()
-	context = {'test': branches}
+	products = reposadocommon.getProductInfo()
+	context = {'test': products}
 	return render(request, 'updates/updats_list.html', context)

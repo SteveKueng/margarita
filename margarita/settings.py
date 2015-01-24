45bc741bb599056c73863b37a05dfc5a74196a66
SteveKueng <kuengst@StevesMacBookPro.local>
2015-01-25 00:17:35 +0100

    fix

--- margarita/settings.py
+++ margarita/settings.py
@@ -67,11 +67,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'site_static'),
 )
 
 TEMPLATE_DIRS = (
